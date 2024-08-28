fb94dfdf6903eba4d461440ca57edf0f52f1bfb1
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-08-28 23:37:38 +0800

    yea

--- PIMENTEL NUMBA WAN/frontend/src/pages/Category.jsx
+++ PIMENTEL NUMBA WAN/frontend/src/pages/Category.jsx
@@ -21,17 +21,17 @@ useEffect(() => {
 console.log(new_collection);
 console.log("wew");
 return (
-      <section className="max_padd_container py-12 xl:py-28">
+      <section className="">
         <div>
           <div>
-            <img src={banner} alt=""  className="block my-7 mx-auto"/>
+            <img src={banner} alt=""  className=""/>
           </div>
-          <div className="flexBetween my-8 mx-2">
-            <h5><span className="font-bold">Products</span></h5>
+          <div className="f">
+            <h5><span className="">Products</span></h5>
             {/* <div className="flexBetween max-sm:p-4 gap-x-4 px-8 py-3 rounded-5xl ring-1 ring-slate-900/5">sort by <MdOutlineKeyboardArrowDown /></div> */}
           </div>
           {/* container} */}
-          <div className="grid grid-cols-1 xs:grid-cols-2 md:gtid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="">
             {new_collection.map((item) => {
               if(category === item.category){
                 return <Item 
@@ -45,7 +45,7 @@ return (
               }
             })}
           </div>
-          <div className="mt-16 text-center">
+          <div className="">
             {/* <button className="btn_dark_rounded">Load More</button> */}
           </div>
         </div>
